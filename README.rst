5e182e6cb5e8807b32a6febdb3ba8fbe234c9539
Rodrigo Campos <rodrigo@sdfg.com.ar>
2013-09-16 20:44:38 +0200

    Initial commit
    Signed-off-by: Rodrigo Campos <rodrigo@sdfg.com.ar>

---
+++ README.rst
@@ -0,0 +1,24 @@
+This is just a collection of scripts that I might have found useful at some
+moment. All of them are relased under the BOLA_ license.
+
+.. _BOLA: http://blitiri.com.ar/p/bola/
+
+If you have any comments, patches or suggestions let me know at:
+rodrigo@sdfg.com.ar. Please keep in mind that most should be a 10 minutes hack
+(or even less :D), so the code might not be the prettiest one :-)
+
+Here is a list of some of the scripts you can find:
+
+ * ``notifyme``: Run any command and open an X window to notify when it
+   finishes. You can also snooze for 1 minute.
+
+ * ``crous-login``: The CROUS_ students residence force you to fill a form every
+   time you want to login and every N hours. This scripts just fills the forms
+   for you whenever needed.
+
+ * ``batrat``: Check if the system is running on AC or batteries and print to
+   stdout whenever the system switches from AC to BAT or viceversa. Uses inotify
+   to detect switches.
+
+
+.. _CROUS: http://www.crous-strasbourg.fr
---
+++ crous-login.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+"""
+crous-login: Fill the forms needed to login to the crous osiris network.
+Rodrigo Campos <rodrigo@sdfg.com.ar>
+"""
+
+import requests
+import time
+import subprocess
+
+USER="XXX"
+PASS="YYY"
+
+
+def login(user, password):
+
+    #subprocess.check_call("killall dhclient ; dhclient -v eth0", shell=True)
+    time.sleep(1)
+
+    r = requests.get("http://google.com")
+    if not "/webauth/loginprocess" in r.text:
+        print "Not the expected form. Already conected ?"
+        return
+
+    print "Got the form..."
+
+    payload = { "user": user, "pass": password }
+    r = requests.post("http://google.com/webauth/loginprocess", data=payload)
+
+    # Print just in case it says some wierd error
+    print r.text
+
+    # Wait till the access is granted
+    while 'granted' not in r.text.lower():
+        # Two seconds is the time used in the META header to refresh, so we wait
+        # the same (and if they change it, fuck it :D)
+        time.sleep(2)
+        r = requests.get("http://google.com/webauth/statusprocess")
+
+    print "granted! Waiting to activate..."
+    time.sleep(10)
+    #subprocess.check_call("killall dhclient ; dhclient -v eth0", shell=True)
+
+def connect(use, password):
+
+    while True:
+        try:
+            login(USER, PASS)
+        except:
+            print "Error while trying to login, trying again"
+            time.sleep(2)
+            continue
+
+        break
+
+def are_connected():
+
+    try:
+        subprocess.check_call("ping -c 2 8.8.8.8 > /dev/null", shell=True)
+        return True
+    except:
+        return False
+
+if __name__ == "__main__":
+
+    while True:
+        if not are_connected():
+            print "Not connected! Trying to connect..."
+            connect(USER, PASS)
+
+        try:
+            time.sleep(60)
+        except KeyboardInterrupt:
+            print "Checking connection again..."
+
